a9a6b5882ba5bd0af698dc10b858fe50abdb2f6e
s8jjmeye <s8jjmeye@stud.uni-saarland.de>
2021-05-20 16:33:37 +0200

    matmul & sparsemul working

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,27 @@
-#include <stdio.h>
+#include <iostream>
 #include "nn.h"
 
+extern float* mem_alloc(int32_t size) {
+    return (float*) malloc(sizeof(float) * size);
+}
+
 int main() {
-    printf("%d\n", get_42());
+    float arr1[] = {    2.f,   1.f,
+                        2.f,   3.f};
+    int arr2[] =   {    0,      1,
+                        1,      2};
+
+    float arr3[] = {    3.f,   2.f,   1.f,
+                        4.f,   5.f,   6.f,
+                        9.f,   8.f,   7.f};
+
+    anydsl::Array<float> dslarr1(sizeof(float) * 9);
+    anydsl::Array<int> dslarr2(sizeof(float) * 9);
+    anydsl::Array<float> dslarr3(sizeof(float) * 9);
+    anydsl_copy(0, arr1, 0, 0, dslarr1.data(), 0, 9 * sizeof(float));
+    anydsl_copy(0, arr2, 0, 0, dslarr2.data(), 0, 9 * sizeof(int));
+    anydsl_copy(0, arr3, 0, 0, dslarr3.data(), 0, 9 * sizeof(float));
+
+    mat_test(&dslarr1, &dslarr2, &dslarr3);
     return 0;
 }
