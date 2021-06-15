a7862f754e98b3989efe776f5ea1aa1b17d9c45c
s8jjmeye <s8jjmeye@stud.uni-saarland.de>
2021-06-15 22:27:58 +0200

    iteration changes

--- src/interface.h
+++ src/interface.h
@@ -6,11 +6,8 @@
 extern "C" {
 #endif
 
-void sparse_mult_test(anydsl::Array<float>*, anydsl::Array<int>*, anydsl::Array<float>*);
-void create_conv_test(anydsl::Array<float>*, anydsl::Array<float>*);
 void image_kernel_test(anydsl::Array<float>* kernel, anydsl::Array<uint8_t>* in, anydsl::Array<uint8_t>* out);
 void sres(anydsl::Array<uint8_t>* in, int32_t width, int32_t height, anydsl::Array<uint8_t>* out, anydsl::Array<float>* kernels, float* biases);
-void offset_matrix_test(anydsl::Array<float>* Buffer, float* biases);
 
 #ifdef __cplusplus
 }
--- src/test.cpp
+++ src/test.cpp
@@ -4,9 +4,7 @@
 #include "image.h"
 #include "nn.h"
 
-// void sparseMult();
 void imageTest();
-// void create_conv();
 void superres(std::string path, int width, int height);
 
 int main() {
@@ -68,43 +66,6 @@ void superres(std::string path, int width, int height) {
     result.release();
     free(biases);
 }
-/*
-void create_conv() {
-    float arr1[] = {    4.f,    3.f,    2.f,    1.f,
-                        5.f,    6.f,    7.f,    8.f,
-                        12.f,   11.f,   10.f,   9.f,
-                        13.f,   14.f,   15.f,   16.f};
-    float arr3[] = {    1.f,    1.f,   1.f,
-                        1.f,    1.f,   1.f,
-                        1.f,    1.f,   1.f};
-
-    anydsl::Array<float> dslarr1(sizeof(float) * 16);
-    anydsl::Array<float> dslarr3(sizeof(float) * 9);
-    anydsl_copy(0, arr1, 0, 0, dslarr1.data(), 0, 16 * sizeof(float));
-    anydsl_copy(0, arr3, 0, 0, dslarr3.data(), 0, 9 * sizeof(float));
-
-    create_conv_test(&dslarr1, &dslarr3);
-}
-
-void sparseMult() {
-    float arr1[] = {    2.f,   1.f,
-                        2.f,   3.f};
-    int arr2[] =   {    0,      1,
-                        1,      2};
-
-    float arr3[] = {    3.f,   2.f,   1.f,
-                        4.f,   5.f,   6.f,
-                        9.f,   8.f,   7.f};
-
-    anydsl::Array<float> dslarr1(sizeof(float) * 9);
-    anydsl::Array<int> dslarr2(sizeof(float) * 9);
-    anydsl::Array<float> dslarr3(sizeof(float) * 9);
-    anydsl_copy(0, arr1, 0, 0, dslarr1.data(), 0, 9 * sizeof(float));
-    anydsl_copy(0, arr2, 0, 0, dslarr2.data(), 0, 9 * sizeof(int));
-    anydsl_copy(0, arr3, 0, 0, dslarr3.data(), 0, 9 * sizeof(float));
-
-    sparse_mult_test(&dslarr1, &dslarr2, &dslarr3);
-}*/
 
 void imageTest() {
     float kernel1[] = { .0f,    .0f,    .0f,     .0f,     .0f,     .0f,    .0f,    .0f,    .5f,
